8621305675ce7a6e0c6e032f6ad92abc1d51fd45
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-26 02:29:34 +0530

    add style

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const expressLayouts=require('express-ejs-layouts');
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+//extract styles and scripts from the subpages to layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //use express router
 app.use('/',require("./routes"))
 app.set('view engine','ejs');
