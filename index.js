11e06fc861b1f65d5f8471e28b03e759bd4eb559
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-26 02:06:25 +0530

    add static

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
+const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 //use express router
 app.use('/',require("./routes"))
 app.set('view engine','ejs');
 app.set('views',"./views");
 
-const port=8000;
 app.listen(port,function(err){
     if(err)
         console.log(`Error: ${err}`);
