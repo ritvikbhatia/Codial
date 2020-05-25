031fa4e2d1a076f04557f451d6069efe10355afa
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-25 15:18:44 +0530

    add another route

---
+++ controllers/usersController.js
@@ -0,0 +1,5 @@
+module.exports.profile=function(req,res){
+    console.log(" users controller loaded");
+    return res.end("User Profile");
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const homeController=require("../controllers/homeControllers");
 console.log("router loaded");
 router.get('/',homeController.home);
+router.use('/users',require("./users"));
 
 module.exports=router;
