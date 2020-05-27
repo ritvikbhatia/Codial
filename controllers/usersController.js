50fce72605913af593477d32ea632874d5c16ab4
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-27 12:33:06 +0530

    add signup

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,5 @@
+const User=require('../models/users');
+
 module.exports.profile=function(req,res){
     console.log(" users controller loaded");
     return res.end("User Profile");
@@ -9,3 +11,47 @@ module.exports.dp=function(req,res){
     return res.end("Display picture");
     
 }
+module.exports.signup=function(req,res){
+    console.log(" sign up controller loaded");
+    return res.render('signup.ejs',{
+        'title':'create account'
+    })
+    
+};
+module.exports.signin=function(req,res){
+    console.log(" sign in controller loaded");
+    return res.render('signin.ejs',{
+        'title':'Login'
+    })
+};
+
+module.exports.create=function(req,res){
+    console.log('create');
+    
+    if(req.body.password==req.body.ConfirmPassword)
+    {
+        User.findOne({email:req.body.email},function(err,user){
+            if(err) console.log(err);
+            
+            if(user)
+                return res.redirect('back');
+            else{
+                User.create(req.body,function(err,user){
+                   if(err)
+                        console.log(err);
+                   return res.redirect('signin');
+                });
+                
+                }
+        })
+            
+        
+    }
+    else
+        res.redirect('back');
+    };
+module.exports.createSession=function(req,res){
+    return res.render('signin.ejs',{
+        'title':'Login'
+    }) 
+};
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
+const cookieParser=require('cookie-parser');
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
 
+app.use(cookieParser());
+app.use(express.urlencoded());
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+
 //extract styles and scripts from the subpages to layout
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
