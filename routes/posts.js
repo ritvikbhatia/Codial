02bde32fd1af8b4532f4330a67830337db03c862
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-29 17:36:14 +0530

    add authentication for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express=require('express');
+const passport=require('passport');
 const router=express.Router();
 const postsController=require("../controllers/postsControllers");
 router.get('/comments',postsController.comments);
-router.post('/createpost',postsController.createpost);
+router.post('/createpost',passport.checkAuthentication, postsController.createpost);
 module.exports=router;
