ec0dc5d37ae40dbaae5ab04a0aa8078dbaa43f9e
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-30 18:10:01 +0530

    add friends feature

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const User = require('../models/user');
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
@@ -21,10 +21,14 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
-        });
+        User.find({},function(err,users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users:users
+            });
+        })
+        
     })
 
 }
