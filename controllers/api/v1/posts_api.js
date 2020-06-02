34854e54cf73f43ac1de6f38334f266617aa864a
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-06-03 02:03:32 +0530

    add api features

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,48 @@
-module.exports.index=function(req,res){
+const Post=require('../../../models/post')
+const Comment=require('../../../models/comment')
+module.exports.index= async function(req,res){
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    });
+    
    return res.json(200,{
         message:"list of posts",
-        posts:[]
+        posts:posts
     })
+}
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let post = await Post.findById(req.params.id);
+
+        // if (post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+
+            // req.flash('success', 'Post and associated comments deleted!');
+             return res.status(200).json({
+                message:"success"
+            })
+
+            
+        // }else{
+        //     req.flash('error', 'You cannot delete this post!');
+        //     return res.redirect('back');
+        // }
+
+    }catch(err){
+        return res.status(500).json({
+            message:"error"
+        })
+    }
+    
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const posts_api=require('../../../controllers/api/v1/posts_api');
 
 router.get('/',posts_api.index);
+router.delete('/:id',posts_api.destroy);
 
 module.exports = router;
